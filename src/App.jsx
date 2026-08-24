c3f08df218b10471e6fe48ab0a32ee43d6679fee
denis springinsfeld <denis.springinsfeld@unilim.fr>
2026-08-24 17:53:45 +0200

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import img from "./assets/vite.svg";
+
 function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <img src={img} alt="Vite Logo" className="ml-4 h-12 w-12 animate-spin" />
     </div>
   );
 }
